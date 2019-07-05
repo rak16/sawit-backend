866bb12652c4f79a343fbe35239de3280c35589b
rak16 <ravishahmad16@gmail.com>
2019-07-06 01:21:44 +0530

    Add boilerplate

---
+++ config/config_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    CLIENT_URL:'http://localhost:3000/',
+    SERVER_URL:'http://localhost:5000/',
+    COOKIE_SECRET:'vjhdbhebchcbsvscxfcss',
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const config = require('./config/config.js');
+
+app.use(express.static('public'));
+
+app.use(
+    bodyParser.urlencoded({
+        limit: '200mb',
+        parameterLimit: 1000000,
+        extended: true
+    })
+);
+
+app.use(
+    bodyParser.json({
+        limit: '200mb',
+    })
+);
+
+app.use(cors());
+app.set('port', process.env.PORT || 5000);
+app.use(express.static('public'));
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [
+            config.COOKIE_SECRET
+        ]
+    })
+);
+
+app.listen(app.get('port'), err => {
+    if (err) {
+        console.log('Failed to start the server.');
+        console.log(err);
+    } else {
+        console.log(`Server running on port ${app.get('port')}`);
+    }
+});
