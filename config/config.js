3adef45bd79160afc22900a7c8899098c95dacd3
rak16 <ravishahmad16@gmail.com>
2019-07-07 18:02:17 +0530

    Fix formatting

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./config_prod');
-} else{
+} else {
     module.exports = require('./config_dev');
 }
