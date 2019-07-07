9e46ac1a0940a1ea21d0ce0219ab0e40c76d290f
rak16 <ravishahmad16@gmail.com>
2019-07-07 17:47:36 +0530

    Update dev config and fix formatting

--- config/config_dev.js
+++ config/config_dev.js
@@ -1,5 +1,6 @@
 module.exports = {
-    CLIENT_URL:'http://localhost:3000/',
-    SERVER_URL:'http://localhost:5000/',
-    COOKIE_SECRET:'vjhdbhebchcbsvscxfcss',
-}
+    CLIENT_URL: 'http://localhost:3000/',
+    SERVER_URL: 'http://localhost:5000/',
+    COOKIE_SECRET: 'hunter1',
+    JWT_SECRET_KEY: 'hunter2'
+};
--- config/config_prod.js
+++ config/config_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CLIENT_URL:process.env.CLIENT_URL,
-    SERVER_URL:process.env.SERVER_URL,
-    COOKIE_SECRET:process.env.COOKIE_SECRET
-}
+    CLIENT_URL: process.env.CLIENT_URL,
+    SERVER_URL: process.env.SERVER_URL,
+    COOKIE_SECRET: process.env.COOKIE_SECRET
+};
